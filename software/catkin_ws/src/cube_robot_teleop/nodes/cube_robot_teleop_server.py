93db2c0fa2921860790733d1cec1a7bece6017fd
yana <yanchik_vau@mail.ru>
2019-09-06 17:57:11 +0300

    server teleop updated

--- software/catkin_ws/src/cube_robot_teleop/nodes/cube_robot_teleop_server.py
+++ software/catkin_ws/src/cube_robot_teleop/nodes/cube_robot_teleop_server.py
@@ -5,13 +5,49 @@ import SocketServer
 import json
 import cgi
 import rospy
-from geometry_msgs.msg import Twist
+from sys import argv
+from std_msgs.msg import Float32
 
-pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+SERVER_NODE = None
 
+def constrain(value, low, high):
+    if value < low:
+        value = low
+    elif value > high:
+        value = high
+    return value
 
-def vels(target_linear_vel, target_angular_vel):
-    return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel, target_angular_vel)
+class ServerNode:
+    def __init__(self):
+        rospy.init_node('cube_robot_teleop_server')
+        self.name = rospy.get_name()
+        rospy.loginfo("{0} started".format(self.name))
+        self.maxLinVel = float(rospy.get_param('~max_lin_vel', 3.82))
+        self.maxAngVel = float(rospy.get_param('~max_ang_vel', 3.82))
+
+        self.linearVelocity = 0.0
+        self.angularVelocity = 0.0
+
+        self.linPub = rospy.Publisher("lin_vel", Float32, queue_size=1)
+        self.angPub = rospy.Publisher("ang_vel", Float32, queue_size=1)
+
+    def publish_angular(self):
+        self.angPub.publish(self.angularVelocity)
+
+    def publish_linear(self):
+        self.linPub.publish(self.linearVelocity)
+
+    def set_linear_velocity(self, vel):
+        self.linearVelocity = constrain(vel, -self.maxLinVel, self.maxLinVel)
+
+    def set_angular_velocity(self, vel):
+        self.angularVelocity = constrain(vel, -self.maxAngVel, self.maxAngVel)
+
+    def show_info(self):
+            rospy.loginfo(self.twist_msg())
+
+    def twist_msg(self):
+        return "Vel:\tlinear vel %s\t angular vel %s " % (self.linearVelocity, self.angularVelocity)
 
 
 class Server(BaseHTTPRequestHandler):
@@ -41,17 +77,12 @@ class Server(BaseHTTPRequestHandler):
         # read the message and convert it into a python dictionary
         length = int(self.headers.getheader('content-length'))
         message = json.loads(self.rfile.read(length))
-        print(message)
-        twist = Twist()
-        twist.linear.x = message['lin']
-        twist.linear.y = 0.0
-        twist.linear.z = 0.0
-        twist.angular.x = 0.0
-        twist.angular.y = 0.0
-        twist.angular.z = message['ang']
-        pub.publish(twist)
-        print vels(twist.linear.x, twist.angular.z)
-        # add a property to the object, just to mess with data
+        rospy.loginfo(message)
+        SERVER_NODE.set_linear_velocity(message['lin'])
+        SERVER_NODE.set_angular_velocity(message['ang'])
+        SERVER_NODE.show_info()
+        SERVER_NODE.publish_angular()
+        SERVER_NODE.publish_linear()
         message['received'] = 'ok'
 
         # send the message back
@@ -62,16 +93,12 @@ class Server(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=Server, port=8008):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-
-    print 'Starting httpd on port %d...' % port
+    print('Starting httpd on port %d...' % port)
     httpd.serve_forever()
 
 
 if __name__ == "__main__":
-    from sys import argv
-
-    rospy.init_node('cube_robot_teleop_server')
-
+    SERVER_NODE = ServerNode()
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
