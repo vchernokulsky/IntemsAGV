5dfcdc07d40f4e328928bd43edac51695b4327bc
yana <yanchik_vau@mail.ru>
2020-01-22 10:36:55 +0300

    app begin

---
+++ software/stm_setup/lib/DecimalInput.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+class DecimalInput extends StatefulWidget {
+  final String title;
+  final double minValue;
+  final double maxValue;
+
+  const DecimalInput({Key key, this.title, this.minValue, this.maxValue})
+      : super(key: key);
+  _DecimalInput createState() =>
+      _DecimalInput(title, minValue, maxValue);
+}
+
+class _DecimalInput extends State<DecimalInput> {
+  _DecimalInput(this.title, this.minValue, this.maxValue);
+
+  final String title;
+
+  final double minValue;
+  final double maxValue;
+
+  String errorMsg;
+
+  void initState() {
+    super.initState();
+    errorMsg = "";
+  }
+
+  void numberValidate(String numStr) {
+    RegExp regExp = new RegExp(
+      r'^(\d*\.)?\d+$',
+      caseSensitive: false,
+      multiLine: false,
+    );
+    setState(() {
+      if (regExp.hasMatch(numStr)) {
+        double num = double.parse(numStr);
+
+        if (num >= minValue && num <= maxValue) {
+          errorMsg = "";
+        } else {
+          errorMsg = "out of range(should be between $minValue and $maxValue)";
+        }
+      }
+      else{
+        errorMsg = "wrong format(should be decimal)";
+      }
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: "$title"),
+            keyboardType: TextInputType.number,
+            onSubmitted: (text) {
+              numberValidate(text);
+            },
+            inputFormatters: <TextInputFormatter>[
+              WhitelistingTextInputFormatter(RegExp(r'\d+|.|,'))
+            ], // Only numbers can be entered
+          ),
+          Text(
+            "$errorMsg",
+            style: TextStyle(
+                color: Colors.red.withOpacity(1.0),
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ software/stm_setup/lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+import 'DecimalInput.dart';
+import 'NumericInput.dart';
+
+class Counter extends StatefulWidget {
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  static const int ROBOT_GEOM = 0;
+  static const int ROBOT_DRIVE = 1;
+
+  static const String RADIUS_ERROR_MSG = "Wrong radius value";
+
+  int curItem;
+  String radiusErrorMsg;
+  double val;
+
+  void initState() {
+    super.initState();
+    val = 0;
+    curItem = ROBOT_GEOM;
+    radiusErrorMsg = "";
+  }
+
+  void change() {
+    setState(() {
+      val += 1;
+    });
+  }
+
+  void numberValidate(String numStr) {
+    int maxValue = 100;
+    int minValue = 1;
+    int num = int.parse(numStr);
+    setState(() {
+      if(num >= minValue && num <= maxValue){
+        radiusErrorMsg = "";
+      } else {
+        radiusErrorMsg = RADIUS_ERROR_MSG;
+      }
+    });
+  }
+
+  void setWidgets(int widget) {
+    setState(() {
+      curItem = widget;
+    });
+    Navigator.pop(context);
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DiffDriveSTM"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Visibility(
+              visible: curItem == ROBOT_GEOM,
+              child:  Column(
+                children: <Widget>[
+                  NumericInput(
+                    title: "Wheel radius(mm)",
+                    minValue: 10,
+                    maxValue: 100,
+                  ),
+                  NumericInput(
+                    title: "Wheel separation(mm)",
+                    minValue: 10,
+                    maxValue: 100,
+                  ),
+                  DecimalInput(
+                    title: "max linear velocity",
+                    minValue: 0.1,
+                    maxValue: 15.0,
+                  ),
+                  DecimalInput(
+                    title: "max angular velocity",
+                    minValue: 0.1,
+                    maxValue: 15.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text("DiffDriveSTM"),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Robot geometry'),
+              onTap: () => setWidgets(ROBOT_GEOM),
+            ),
+            ListTile(
+              title: Text('Robot drive'),
+              onTap: () => setWidgets(ROBOT_DRIVE),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Container(
+          child: Counter(),
+        ),
+      ),
+    );
+  }
+}
+
+Future<void> main() async {
+  runApp(MyApp());
+}
