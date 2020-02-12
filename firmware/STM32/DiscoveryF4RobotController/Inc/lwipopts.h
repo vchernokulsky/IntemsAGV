d2a7c77153c149314448d2bfd6e940d6d435a18b
yana <yanchik_vau@mail.ru>
2020-02-12 18:31:37 +0300

    set arp timeout

--- firmware/STM32/DiscoveryF4RobotController/Inc/lwipopts.h
+++ firmware/STM32/DiscoveryF4RobotController/Inc/lwipopts.h
@@ -53,6 +53,8 @@
 #define MEM_SIZE 2048
 /*----- Default Value for MEMP_NUM_TCP_SEG: 16 ---*/
 #define MEMP_NUM_TCP_SEG 24
+/*----- Default Value for ARP_MAXAGE: 300 ---*/
+#define ARP_MAXAGE 6000
 /*----- Value in opt.h for LWIP_ETHERNET: LWIP_ARP || PPPOE_SUPPORT -*/
 #define LWIP_ETHERNET 1
 /*----- Value in opt.h for LWIP_DNS_SECURE: (LWIP_DNS_SECURE_RAND_XID | LWIP_DNS_SECURE_NO_MULTIPLE_OUTSTANDING | LWIP_DNS_SECURE_RAND_SRC_PORT) -*/
