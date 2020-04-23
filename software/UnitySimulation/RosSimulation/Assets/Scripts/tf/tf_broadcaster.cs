bbc45d4961d72142f0147a1cd5ff89a270e1eaf6
yana <yanchik_vau@mail.ru>
2020-04-23 12:30:24 +0300

    create tf broadcaster

---
+++ software/UnitySimulation/RosSimulation/Assets/Scripts/tf/tf_broadcaster.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RosSharp.RosBridgeClient.MessageTypes.Geometry;
+using RosSharp.RosBridgeClient.MessageTypes.Rosapi;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class tf_broadcaster : UnityPublisher<MessageTypes.Geometry.TransformStamped>
+    {
+        private TransformStamped msg;
+
+        private Queue<TransformStamped> msgQueue;
+        
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            Topic = "/tf";
+            msgQueue = new Queue<TransformStamped>();
+            base.Start();
+        }
+
+        // Update is called once per frame
+        protected void FixUpdate()
+        {
+            if (msgQueue.Count > 0)
+            {
+                msg = msgQueue.Dequeue();
+                Publish(msg);
+            }
+        }
+
+        public void SedTransform(TransformStamped tf_msg)
+        {
+           msgQueue.Enqueue(tf_msg);
+        }
+        
+        
+    }
+}
